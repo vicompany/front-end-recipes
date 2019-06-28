0bc5344aac65f86fafa54b8fa7b81eeb0ddc68fe
Sebastiaan van Kerkfort <ba55ie@users.noreply.github.com>
2019-06-28 14:46:29 +0200

    Use babel-plugin-transform-async-to-promises (#6)
    * Use babel-plugin-transform-async-to-promises
    
    * More async example code

--- rollup-itscss/source/scripts/modules/module-a.mjs
+++ rollup-itscss/source/scripts/modules/module-a.mjs
@@ -1,9 +1,17 @@
+const delay = (timeout = 500) => new Promise(r => setTimeout(r, timeout));
+
+const asyncMessage = async msg => `Async: ${msg}`;
+
 export const msg = async (text, el = document.querySelector('main')) => {
 	const p = document.createElement('p');
 
-	p.textContent = text;
+	p.textContent = 'Loading...';
 
 	el.appendChild(p);
+
+	await delay(2500);
+
+	p.textContent = await asyncMessage(text);
 
 	return p;
 };
