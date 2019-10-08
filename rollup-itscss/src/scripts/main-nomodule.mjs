6b0950bac0edf994dbda4a40efea097a919c39a7
Sebastiaan van Kerkfort <sebastiaan@vicompany.nl>
2019-10-08 10:44:20 +0200

    Remove dom4 package

--- rollup-itscss/src/scripts/main-nomodule.mjs
+++ rollup-itscss/src/scripts/main-nomodule.mjs
@@ -1,6 +1,4 @@
-// Import any polyfills not in core-js here before main (e.g. whatwg-fetch)...
-import 'dom4';
-import 'core-js/es/promise'; // Using only async functions won't enable this polyfill.
+// Import any polyfills not in core-js here before main (e.g. whatwg-fetch, dom4).
 
 import { main } from './main.mjs';
 
