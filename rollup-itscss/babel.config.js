b67b79153c16542f7070264a41816a432084d5b6
ba55ie <ba55ie@users.noreply.github.com>
2019-11-14 09:52:01 +0100

    Use Babel `env`  option

--- rollup-itscss/babel.config.js
+++ rollup-itscss/babel.config.js
@@ -1,13 +1,41 @@
 module.exports = {
-	presets: [
-		['@babel/preset-env', {
-			useBuiltIns: 'usage',
-			corejs: 3,
-			exclude: [
-				// Edge does not pass the Promise test and thus includes
-				// this polyfill: https://github.com/zloirock/core-js/issues/579#issuecomment-504325213
-				'es.promise',
+	env: {
+		modern: {
+			presets: [
+				['@babel/preset-env', {
+					useBuiltIns: 'usage',
+					corejs: 3,
+					exclude: [
+						// Edge does not pass the Promise test and thus includes
+						// this polyfill: https://github.com/zloirock/core-js/issues/579#issuecomment-504325213
+						// Therefor we have to exclude it.
+						'es.promise',
+					],
+					debug: true,
+				}],
 			],
-		}],
-	],
+		},
+
+		legacy: {
+			presets: [
+				['@babel/preset-env', {
+					useBuiltIns: 'usage',
+					corejs: 3,
+					exclude: [
+						'@babel/plugin-transform-async-to-generator',
+						'@babel/plugin-transform-regenerator',
+					],
+					targets: {
+						browsers: [
+							'ie 11',
+						],
+					},
+					debug: true,
+				}],
+			],
+			plugins: [
+				'babel-plugin-transform-async-to-promises',
+			],
+		},
+	},
 };
